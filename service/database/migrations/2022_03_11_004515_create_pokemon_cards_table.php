2ffdbea8e8bff63d9fca9dff91d0be5daefbbb54
onnsui <ryo.sato@roxx.co.jp>
2022-03-13 13:22:49 +0900

    second commit

--- service/database/migrations/2022_03_11_004515_create_pokemon_cards_table.php
+++ service/database/migrations/2022_03_11_004515_create_pokemon_cards_table.php
@@ -14,7 +14,16 @@ class CreatePokemonCardsTable extends Migration
     public function up()
     {
         Schema::create('pokemon_cards', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->unsignedInteger('user_id')->comment('保有してるユーザーID');
+            $table->string('name', 255)->comment('カード名');
+            $table->unsignedInteger('expansion_mark_id')->comment('エキスパンションマークID');
+            $table->unsignedInteger('regulation_id')->comment('レギュレーションマークID');
+            $table->integer('list_no')->comment('リスト番号');
+            $table->unsignedInteger('rarity_id')->comment('レアリティ');
+            $table->dateTime('created')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->datetime('modified')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+
             $table->timestamps();
         });
     }
